94d35a335422ccf0ba12f612f298c6363990352a
ATan <z.t.liu@foxmail.com>
2020-08-31 22:20:52 +0800

    Redis引入以及分布式锁

--- gmall-manage-service/src/main/java/com/fiberhome/gmall/GmallManageServiceApplication.java
+++ gmall-manage-service/src/main/java/com/fiberhome/gmall/GmallManageServiceApplication.java
@@ -1,4 +1,4 @@
-package com.fiberhome.gmall.manage;
+package com.fiberhome.gmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gmall-manage-service/src/main/java/com/fiberhome/gmall/manage/service/impl/SkuServiceImpl.java
+++ gmall-manage-service/src/main/java/com/fiberhome/gmall/manage/service/impl/SkuServiceImpl.java
@@ -1,6 +1,7 @@
 package com.fiberhome.gmall.manage.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 import com.fiberhome.gmall.bean.PmsSkuAttrValue;
 import com.fiberhome.gmall.bean.PmsSkuImage;
 import com.fiberhome.gmall.bean.PmsSkuInfo;
@@ -10,9 +11,13 @@ import com.fiberhome.gmall.manage.mapper.PmsSkuImageMapper;
 import com.fiberhome.gmall.manage.mapper.PmsSkuInfoMapper;
 import com.fiberhome.gmall.manage.mapper.PmsSkuSaleAttrValueMapper;
 import com.fiberhome.gmall.service.SkuService;
+import com.fiberhome.gmall.util.RedisUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author ATan147
@@ -33,6 +38,9 @@ public class SkuServiceImpl implements SkuService {
 
     @Autowired
     PmsSkuImageMapper pmsSkuImageMapper;
+
+    @Autowired
+    RedisUtil redisUtil;
 
 
     @Override
@@ -62,21 +70,54 @@ public class SkuServiceImpl implements SkuService {
 
     @Override
     public PmsSkuInfo getSkuById(String skuId) {
+        PmsSkuInfo pmsSkuInfo = new PmsSkuInfo();
+        Jedis jedis = redisUtil.getJedis();
+        String skuKey = "sku:" + skuId + ":info";
+        String skuJson = jedis.get(skuKey);
+        if (StringUtils.isNoneBlank(skuJson)) {
+            pmsSkuInfo = JSON.parseObject(skuJson, PmsSkuInfo.class);
+        } else {
+            String lockKey = "sku:" + skuId + ":lock";
+            String token = UUID.randomUUID().toString();
+            String OK = jedis.set(lockKey, token, "nx", "px", 6000);
+            if (StringUtils.isNotBlank(OK)&&OK.equals("OK")){
+                pmsSkuInfo = getSkuByIdFromDb(skuId);
+                if (pmsSkuInfo != null) {
+                    jedis.set(skuKey, JSON.toJSONString(pmsSkuInfo));
+                } else {
+                    //防止缓存穿透、将null值赋值给redis
+                    jedis.setex(skuKey, 1000, JSON.toJSONString(""));
+                }
+                String lockToken = jedis.get(lockKey);
+                if (StringUtils.isNoneBlank()&&lockToken.equals(token)){
+                    // 使用lua脚本保证正确
+                    jedis.del(lockKey);
+                }
+            }else {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return getSkuById(skuId);
+            }
+        }
+        jedis.close();
+        return pmsSkuInfo;
+    }
+
+    public PmsSkuInfo getSkuByIdFromDb(String skuId) {
         PmsSkuInfo pmsSkuInfo = pmsSkuInfoMapper.selectByPrimaryKey(skuId);
-
-
         PmsSkuImage pmsSkuImage = new PmsSkuImage();
         pmsSkuImage.setSkuId(skuId);
         List<PmsSkuImage> pmsSkuImages = pmsSkuImageMapper.select(pmsSkuImage);
         pmsSkuInfo.setSkuImageList(pmsSkuImages);
-
-
         return pmsSkuInfo;
     }
 
+
     @Override
     public List<PmsSkuInfo> getSkuSaleAttrValueListBySpu(String productId) {
-
         return pmsSkuInfoMapper.selectSkuSaleAttrValueListBySpu(productId);
     }
 }
